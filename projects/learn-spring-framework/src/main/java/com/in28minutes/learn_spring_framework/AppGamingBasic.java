c27c0eb67cfcd146a18d7de49c8aeabd3aecda0c
Shehab Ahmed <shihap.amara@gmail.com>
2025-01-09 10:04:02 +0200

    designed a game runner (tight coupled) {iteration 1}

---
+++ projects/learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/AppGamingBasic.java
@@ -0,0 +1,10 @@
+package com.in28minutes.learn_spring_framework;
+
+public class AppGamingBasic {
+    public static void main(String[] args) {
+        
+        var marioGame = new MarioGame();
+        var gameRunner = new GameRunner(marioGame);
+        gameRunner.run();
+    }
+}
